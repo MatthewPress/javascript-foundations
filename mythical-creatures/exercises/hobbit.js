27e82bcc15ddee1624d990135a0adfdde2da2f74
MatthewPress <press.matt14@gmail.com>
2022-07-12 00:08:43 -0600

    Feat: completed hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,6 +1,30 @@
 class Hobbit {
-  constructor() {
-    this.name = name;
+  constructor(hobbit) {
+    this.name = hobbit.name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+  }
+  celebrateBirthday() {
+    this.age++;
+
+    if (this.age >= 33) {
+      this.adult = true;
+    }
+    if (this.age >= 101) {
+      this.old = true;
+    }
+  }
+  getRing() {
+    if (this.name === "Frodo") {
+      this.hasRing = true;
+      return `Here is the ring!`;
+    }
+    else {
+      this.hasRing = false;
+      return `You can't have it!`;
+    }
   }
 }
+
 module.exports = Hobbit;
